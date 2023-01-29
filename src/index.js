64303e66ee891c4a3f8bb12723607b78ac0a1124
Winston Co <winstonco@live.com>
2023-01-28 21:23:11 -1000

    docker

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import * as dotenv from 'dotenv';
+
+const dotenvPath = path.resolve(
+  process.cwd(),
+  process.env.NODE_ENV ? '.env' : '.env.development'
+);
+
+dotenv.config({
+  path: dotenvPath,
+});
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.static('dist'));
+
+// https://ui.dev/react-router-cannot-get-url-refresh
+app.get('/*', function (req, res) {
+  res.sendFile(path.resolve(process.cwd(), 'dist/index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
