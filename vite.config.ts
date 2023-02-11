500876255c2fb7d4ab0109f1d5389d6b3a709cdb
Winston Co <winstonco@live.com>
2023-02-10 19:55:14 -1000

    update vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // https://stackoverflow.com/questions/69260715/skipping-larger-chunks-while-running-npm-run-build
+    // https://rollupjs.org/configuration-options/#output-manualchunks
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+});
